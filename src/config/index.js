3ec5b95d6fb71d2e83cbe45f3c347850e410137a
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-02-16 15:35:33 -0300

    sumando de a poco testing

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv';
+import logger from '../logs/logger';
 
 dotenv.config();
 
+let mongoDBSRV = process.env.MONGO_ATLAS_URL_TEST || 'mongodb+srv://gonAlmiron:1234@cluster0.ohn3xaw.mongodb.net/test?retryWrites=true&w=majority'
+
+if (
+    process.env.NODE_ENV == 'TEST=INT' ||
+    process.env.NODE_ENV == 'TEST=E2E'
+) {
+    logger.info('Estamos en un test. usando SRV MONGO TEST');
+    mongoDBSRV = process.env.MONGO_ATLAS_URL_TEST || 'mongodb+srv://gonAlmiron:1234@cluster0.ohn3xaw.mongodb.net/test?retryWrites=true&w=majority'
+}
+
+
 export default {
-    MONGO_ATLAS_URL: process.env.MONGO_ATLAS_URL || 'mongodb+srv://gonAlmiron:1234@cluster0.ohn3xaw.mongodb.net/?retryWrites=true&w=majority',
+    MONGO_ATLAS_URL: mongoDBSRV,
     PUERTO: process.env.PUERTO || 3002
 }
