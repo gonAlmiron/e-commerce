901fd1e9dbcd8c6c57036b5fa437c4ee6e47aa7c
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-03-11 12:53:15 -0300

    auth funcionando

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -2,35 +2,14 @@ import {Router} from 'express'
 import { loginController, signUpController, getAllController } from '../controllers/users.controller';
 import passport from 'passport';
 import { passportOptions } from '../services/users.services';
-import logger from '../logs/logger';
 
 
 
 const router = Router();
 
-router.post('/signup', (req, res, next) => {
-  passport.authenticate('signup', passportOptions, (err, user, info) => {
-    console.log('Info SIGNUP');
-    console.log(err, user, info);
-    if (err) {
-      return next(err);
-    }
-    if (!user) return res.status(401).json(info);
+router.post( '/login', passport.authenticate('login', passportOptions), loginController) ;
 
-  
-    logger.info(`Se registró un usuario: ${user.username} \n\n. Ruta /SIGNUP. Metogo POST`)
-
-    res.json({ msg: 'signup OK' });
-  })(req, res, next);
-});
-
-router.post('/login', passport.authenticate('login', passportOptions), (req, res, user) => {
-    logger.info(`Se loge{o} un usuario. Ruta /LOGIN. Metogo POST`)
-    res.json( {
-      msg: `Login OK!!`
-    })
-  },
-);
+router.post('/signup', signUpController);
 
 router.get('/users', getAllController)
 
